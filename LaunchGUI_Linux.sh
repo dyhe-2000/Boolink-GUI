59f976ae31b59c25407b891a3c452e440a4df562
Aravind Karanam <62037160+karavindrao@users.noreply.github.com>
2021-05-21 13:12:51 -0700

    Add files via upload
    Made appropriate X11 changes for LaunchGUI_Linux.sh

--- LaunchGUI_Linux.sh
+++ LaunchGUI_Linux.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+xhost +
+
+# stop the running containers on docker
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+
 # create a volume on the host machine
 sudo docker volume create data-store-loc
 
@@ -9,14 +15,16 @@ data_add=$(sudo docker volume inspect --format '{{ .Mountpoint }}' data-store-lo
 # run the container from the image. include the required options
 #sudo docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v data-store-loc:/usr/src/app aravindraok/boolink:pphys
 
-sudo docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v data-store-loc:/usr/src/app aravindraok/boolink:pphys
+#sudo docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v data-store-loc:/usr/src/app aravindraok/boolink:pphys
 
-#docker run --net=host --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix" -v data-store-loc:/usr/src/app aravindraok/boolink:mplbackend
+sudo docker run --net=host -e DISPLAY=unix$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix" -v data-store-loc:/usr/src/app aravindraok/boolink:pphys
 
 
 # after the GUI is closed, retrieve the data from docker mount volume into a new directory
 mkdir -p simulation_data
 cd simulation_data
 cp -r ${data_add} ./
-
 cd ../
+
+# remove the volume created above
+sudo docker volume rm data-store-loc
